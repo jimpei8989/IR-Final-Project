09a74f6897dd66f7ba27a625d003c38508d3c9b9
andy94077 <andy94077@gmail.com>
2020-06-26 20:51:50 +0800

    Add okapi.py

--- src/okapi.py
+++ src/okapi.py
@@ -6,6 +6,7 @@ from multiprocessing.pool import ThreadPool
 import numpy as np
 import scipy.sparse
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import normalize
 
 from Modules.utils import EventTimer
 from Modules import utils
@@ -25,33 +26,42 @@ def main():
     corpus = Corpus(os.path.join(args.dataDir, 'corpus/'), args.docIDFile, args.usedFeatures)
 
     with EventTimer('Calculate Document Length'):
-        documentLengthes = np.array(list(map(lambda doc : len(doc.split(' ')), corpus)))
+        if not os.path.exists(os.path.join(args.dataDir, 'corpus', 'doc_len.npy')):
+            documentLengthes = np.array(list(map(lambda doc: len(doc.split(' ')), tqdm(corpus))))
+            np.save(os.path.join(args.dataDir, 'corpus', 'doc_len.npy'), documentLengthes)
+        else:
+            documentLengthes = np.load(os.path.join(args.dataDir, 'corpus', 'doc_len.npy'))
         avgDL = np.mean(documentLengthes)
         print(f'> avgDL:\t{avgDL}')
 
     with EventTimer('Load model'):
         model = utils.pickleLoad(os.path.join(args.modelDir, 'model.pkl'))
         tfidf = scipy.sparse.load_npz(os.path.join(args.modelDir, 'tfidf.npz'))
+    
+    with EventTimer('Building TF-IDF'):
+        if not os.path.exists(os.path.join(args.modelDir, 'okapi.npz')):
+            idf = model.idf_.reshape(1, -1)
+            tf = tfidf.multiply(scipy.sparse.csr_matrix(1 / idf))
 
-        idf = model.idf_.reshape(1, -1)
-        tf = tfidf.multiply(scipy.sparse.csr_matrix(1 / idf))
-
-        # TODO: convert to the tfidf
-        documentLengthes = scipy.sparse.csr_matrix(documentLengthes.reshape(-1, 1))
-        row, col = tf.nonzero()
-        data = tf[row, col]
-        data = data * (args.k1 + 1) / (data + args.k1 * (1 - args.b + args.b * documentLengthes[row] / avgDL)) * idf[col]
-        tfidf = scipy.sparse.csr_matrix((data, (row, col)), shape=tf.shape)
-        del row, col, data, tf
+            row, col = tf.nonzero()
+            data = tf.data
+            data = data * (args.k1 + 1) / (data + args.k1 * (1 - args.b + args.b * documentLengthes[row] / avgDL)) * idf[0, col]
+            tfidf = scipy.sparse.csr_matrix((data, (row, col)), shape=tf.shape)
+            scipy.sparse.save_npz(os.path.join(args.modelDir, 'okapi.npz'), tfidf, compressed=False)
+            del row, col, data, tf
+        else:
+            tfidf = scipy.sparse.load_npz(os.path.join(args.modelDir, 'okapi.npz'))
+            
 
     print(f'> TFIDF shape: {tfidf.shape}')
-    print(f'> IDF shape: {idf.shape}')
 
+    tfidf = normalize(tfidf, axis=1)
     # Query
     def evaluate(q):
         query, rel = q
         queryVec = model.transform([query]).toarray()
         queryVec *= (args.k3 + 1) / (args.k3 + queryVec)
+        queryVec = normalize(queryVec, axis=1)
         scores = (tfidf @ queryVec.reshape(-1, 1)).reshape(-1)
         topKIdxes = np.argpartition(-scores, args.topK)[:args.topK]
         topKIdxes = sorted(topKIdxes, key=lambda i:scores[i], reverse=True)
@@ -79,7 +89,7 @@ def parseeArguments():
     parser.add_argument('--k3', type=float, default=3)
     parser.add_argument('--b', type=float, default=0.75)
     parser.add_argument('--topK', type=int, default=1000)
-    parser.add_argument('--numWorkers', type=int, default=8)
+    parser.add_argument('--numWorkers', type=int, default=16)
     return parser.parse_args()
 
 if __name__ == '__main__':
