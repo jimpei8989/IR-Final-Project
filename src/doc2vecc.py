1f7f54c79ed822f2a5c9ac2e74321fc812fe9df2
andy94077 <andy94077@gmail.com>
2020-06-27 15:05:00 +0800

    Add argument in doc2vecc.py

--- src/doc2vecc.py
+++ src/doc2vecc.py
@@ -19,9 +19,13 @@ if __name__ == "__main__":
     parser.add_argument('-dv', '--docvec-file')
     parser.add_argument('-v', '--vocab-file')
     parser.add_argument('-di', '--docid-pkl-file')
-    parser.add_argument('-q', '--query-dirs', nargs=3, help='training query dir, dev query dir, test query, dir')
+    parser.add_argument('-q', '--query-dirs', nargs='+', help='training query dir, dev query dir, test query, dir')
     parser.add_argument('-vs', '--vector-size', type=int, default=128)
     parser.add_argument('-s', '--stemming', action='store_true')
+    parser.add_argument('-r', '--relevance-feedback-steps', type=int, default=2)
+    parser.add_argument('-a', '--alpha', type=float, default=0.8, help='Original ratio for relevance feedback.')
+
+    
     args = parser.parse_args()
     
     assert not (bool(args.query_dirs) ^ bool(args.docid_pkl_file)), 'docid_pkl_file must exist if query_dirs exist.'
@@ -41,13 +45,13 @@ if __name__ == "__main__":
 
     if args.query_dirs:
         idx2id = utils.pickleLoad(args.docid_pkl_file)
-        for query_dir, task in zip(args.query_dirs, ['Training', 'Development', 'Testing']):
+        for query_dir in args.query_dirs:
             query_set = QueryDataset(query_dir)
 
             if args.stemming:
                 queries = PorterStemmer().stem_documents(query_set.queries[:1000])
             else:
                 queries = query_set.queries
-            pred_idx = doc2vecc.get_similar(queries, 1000)
+            pred_idx = doc2vecc.get_similar(queries, 1000, relevance_feedback_steps=args.relevance_feedback_steps, alpha=args.alpha)
             pred_id = idx2id[pred_idx]
-            print(f'\033[32;1m{task} MAP Score: {utils.MAP(query_set.relevantDocuments, pred_id)}\033[0m')
+            print(f'\033[32;1m{query_dir} MAP Score: {utils.MAP(query_set.relevantDocuments, pred_id)}\033[0m')
