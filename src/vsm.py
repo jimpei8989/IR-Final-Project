2406e8b1315e69cc93b9cc78ec809f56995fcef0
Wu-Jun Pei <jimpei8989@gmail.com>
2020-06-28 01:03:16 +0800

    Add relevance feedback, LSI

--- src/vsm.py
+++ src/vsm.py
@@ -6,6 +6,7 @@ from multiprocessing.pool import ThreadPool
 import numpy as np
 import scipy.sparse
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD
 
 from Modules.utils import EventTimer
 from Modules import utils
@@ -19,7 +20,7 @@ def StemmingTokenizer(text):
     return [stemmer.stem(word) for word in word_tokenize(text)]
 
 def main():
-    args = parseeArguments()
+    args = parseArguments()
 
     # mkdir
     os.makedirs(args.modelDir, exist_ok=True)
@@ -56,13 +57,28 @@ def main():
 
     print(f'> TFIDF shape: {tfidf.shape}')
 
+    if args.lsi:
+        with EventTimer('Fitting SVD'):
+            svd = TruncatedSVD(n_components=args.lsiRank, algorithm="arpack")   # arpack is more efficient
+            tfidf = svd.fit_transform(tfidf)
+
     # Query
     def evaluate(q):
         query, rel = q
         queryVec = model.transform([query]).toarray()
-        scores = (tfidf @ queryVec.reshape(-1, 1)).reshape(-1)
-        topKIdxes = np.argpartition(-scores, args.topK)[:args.topK]
-        topKIdxes = sorted(topKIdxes, key=lambda i:scores[i], reverse=True)
+
+        if args.lsi:
+            queryVec = svd.transform(queryVec)
+
+        for generation in range(1 + args.feedbackIterations):
+            scores = (tfidf @ queryVec.reshape(-1, 1)).reshape(-1)
+            topKIdxes = np.argpartition(-scores, args.topK)[:args.topK]
+            topKIdxes = sorted(topKIdxes, key=lambda i:scores[i], reverse=True)
+            if args.lsi:
+                queryVec = args.alpha * queryVec + (1 - args.alpha) * (np.mean(tfidf[topKIdxes[:args.feedbackDocuments]], axis=0, keepdims=True))
+            else:
+                queryVec = args.alpha * queryVec + (1 - args.alpha) * (np.mean(tfidf[topKIdxes[:args.feedbackDocuments]].toarray(), axis=0, keepdims=True))
+
         return utils.MAP([rel], [map(corpus.idx2DocID, topKIdxes)])
 
     with open(os.path.join(args.modelDir, 'result.txt'), 'w') as f:
@@ -76,7 +92,7 @@ def main():
                 print(f'> MAP: {np.mean(APs):.4f}')
                 print(f'{qdir}\t-> MAP: {np.mean(APs):.4f}', file=f)
 
-def parseeArguments():
+def parseArguments():
     parser = ArgumentParser()
     parser.add_argument('--dataDir', default='data/')
     parser.add_argument('--docIDFile', default='data/partial/corpus/docIDs')
@@ -89,7 +105,12 @@ def parseeArguments():
     parser.add_argument('--minDF', type=int, default=100, help='Minimum appearance can a word be adopted')
     parser.add_argument('--maxDF', type=float, default=0.5, help='Maximum frequency can a word be adopted')
     parser.add_argument('--sublinearTF', type=bool, default=True)
+    parser.add_argument('--lsi', action='store_true')
+    parser.add_argument('--lsiRank', type=int, default=128)
     parser.add_argument('--topK', type=int, default=1000)
+    parser.add_argument('--alpha', type=float, default=0.95)
+    parser.add_argument('--feedbackIterations', type=int, default=0)
+    parser.add_argument('--feedbackDocuments', type=int, default=100)
     parser.add_argument('--numWorkers', type=int, default=8)
     return parser.parse_args()
 
